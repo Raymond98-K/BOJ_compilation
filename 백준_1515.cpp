ac5c0c81848c20537e9d8bbc32a902e85bf92359
Raymond98-K <60088470+Raymond98-K@users.noreply.github.com>
2022-01-25 16:13:48 +0900

    Add files via upload

---
+++ 백준_1515.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+	string erased;
+	cin >> erased;
+
+	int checker = 0;
+	int i = 0;
+
+	while(i++<=30000)
+	{
+		cout << i;
+		string testNum = to_string(i);
+
+		for (int j = 0; j < testNum.length(); ++j)
+		{
+			if (testNum.at(j) == erased.at(checker))
+			{
+				checker++;
+			}
+
+			if (checker == erased.length())
+			{
+				cout << i;
+
+				return 0;
+			}
+
+		}
+
+	}
+
+}
